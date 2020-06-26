8a5295c6ab88af7e193f50a7e79a2466c4c0a93a
Haiyang Hong <haiyanghong@Haiyangs-iMac.local>
2020-06-26 01:28:34 -0700

    add AvatarsPanel component

---
+++ client/src/components/AvatarsPanel.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const AvatarsPanel = () => {
+  return (
+    <div className="card">
+      <div className="card-body px-3 px-md-4 py-1 text-nowrap overflow-auto d-flex">
+        <div className="avatar-box d-flex flex-column mr-2">
+          <div className="avatar">
+            <a href="">
+              <img
+                src="https://cdn.dribbble.com/users/2805223/avatars/normal/22a4df5f46555e1c56bc063f52e17155.jpg?1579247942"
+                alt="profilePic"
+                className="img-fluid"
+              />
+            </a>
+          </div>
+          <small className="text-xs text-dark mt-1">Jane Doe</small>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AvatarsPanel;
